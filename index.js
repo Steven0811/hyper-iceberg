1f46c13842a4f6d9929abacc3614d4225b33a142
Steven Ho <shockshocksoul@gmail.com>
2019-01-21 23:43:13 +0800

    ** Fix ** Missed index.js file🤯

--- index.js
+++ index.js
@@ -1,72 +1,83 @@
-"use strict";
-
 // main color
-const mainColor = "#60f8eb";
-const backgroundColor = "#FDFDFD";
-const foregroundColor = "#021923";
+const mainColor = "#67dee4";
+const backgroundColor = "#E7F1F4";
+const foregroundColor = "#013733";
 
+const borderColor = mainColor;
+const cursorColor = mainColor;
+
+const padding = "12px 2px 12px 12px";
+
+const cursorShape = "BLOCK";
 const colors = {
   // Colors
   black: foregroundColor,
   white: "#FFFFFF",
-  red: "#dd2e37",
-  green: "#07c679",
-  blue: "#1B306E",
-  cyan: "#23c1c8",
-  yellow: "#ffd200",
-  magenta: "#5821c7",
-  white: "#c5e1df",
+  red: "#f75f5e",
+  green: "#06c9a3",
+  blue: "#01629d",
+  cyan: "#13b4cb",
+  yellow: "#ffd36b",
+  magenta: "#8032d8",
+  white: "#d5e4e4",
 
   // Light Colors
   lightBlack: foregroundColor,
-  lightRed: "#dd2e37",
-  lightGreen: "#07c679",
-  lightYellow: "#ffd200",
-  lightBlue: "#29467A",
-  lightMagenta: "#5821c7",
-  lightCyan: "#23c1c8",
-  lightWhite: "#FFFFFF"
+  lightRed: "#f75f5e",
+  lightGreen: "#06c9a3",
+  lightYellow: "#ffd36b",
+  lightBlue: "#01629d",
+  lightMagenta: "#8032d8",
+  lightCyan: "#13b4cb",
+  lightWhite: "#d5e4e4"
 };
 
-exports.decorateConfig = config =>
-  Object.assign({}, config, {
-    backgroundColor: backgroundColor,
-    foregroundColor: foregroundColor,
-    borderColor: mainColor,
-    cursorColor: mainColor,
-    cursorShape: "BLOCK",
+exports.decorateConfig = config => {
+  return Object.assign({}, config, {
+    padding,
+    backgroundColor,
+    foregroundColor,
+    borderColor,
+    cursorColor,
+    cursorShape,
     colors,
-    termCSS: `
-            ${config.termCSS || ""}
-            ::selection {
-                 background: ${mainColor} !important;
-            }
-        `,
     // CSS setting
     css: `
       ${config.css || ""}
-
-      ::selection {
-         background: ${mainColor} !important;
+      * {
+      	text-rendering: optimizeLegibility;
+        font-weight: 500;
       }
-
-      .tabs_list .tab_tab .tab_text,
-      .tab_icon {
-        color: ${colors.white};
+      .hyper_main {
+        border: solid 2px ${mainColor};
       }
-
-      .tabs_list .tab_tab.tab_active .tab_text,
-      .tabs_title,
-      .tab_icon:hover {
-        color: ${foregroundColor};
+      .tabs_nav {
+      	background-color: #cfe7ed;
+      }
+      .tab_tab {
+        color: #8e9191;
+        background-color:#cfe7ed;
+				border-color: ${borderColor};
+      }
+      .tab_tab:before {
+      	border: 0;
+      }
+      .tab_tab.tab_active {
+        border: transparent;
         font-weight: bold;
+        color: ${foregroundColor};
+        background-color: ${backgroundColor};
+        .tab_textInner, .tab_icon {
+        svg {
+            fill: ${foregroundColor} !important;
+          }
+        }
       }
-
-      .tab_icon .shape_wom {
-        width: 10px;
-        height: 10px;
-        top: 2px;
-        left: 2px;
-     }
+      .tab_textInner, .tab_icon, .tabs_title {
+        svg {
+          fill: #8e9191 !important;
+        }
+      }
     `
   });
+};
